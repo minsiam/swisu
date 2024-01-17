f07f9567e21531665e48af006191b655d63b91fe
Zihad <zihadmahiuddin@gmail.com>
2024-01-18 01:40:15 +0600

    fix OsuButton background style

--- src/components/osu-button.tsx
+++ src/components/osu-button.tsx
@@ -1,14 +1,13 @@
-import { cn } from '../utils/tailwind';
-
 export interface OsuButtonProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function OsuButton({ className, ...props }: OsuButtonProps) {
     return (
         <div
-            className={cn(
-                'flex min-h-28 cursor-pointer items-center justify-center rounded-xl bg-[length:150%] bg-[position:50%_60%] text-2xl font-semibold text-white drop-shadow-2xl transition-all duration-500 hover:bg-[calc(50%_+_20px)_50%]',
+            className={[
+                'flex h-full min-h-28 w-full cursor-pointer items-center justify-center  rounded-xl text-2xl font-semibold text-white drop-shadow-2xl transition-all duration-500',
                 className,
-            )}
+                'bg-[length:150%] bg-[position:50%_60%] hover:bg-[calc(50%_+_20px)_50%]',
+            ].join(' ')}
             style={{ backgroundImage: 'url("/assets/button-bg.svg")' }}
             {...props}
         />
